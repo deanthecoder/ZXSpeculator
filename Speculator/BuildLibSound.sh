b83efa4b780728fb92cd371bec436c9bab041862
Dean Edis <dean.edis@gmail.com>
2023-12-21 16:09:34 +0000

    Other: We don't need libsoundio builds for Windows.
    ...as NAudio is used instead.

--- Speculator/BuildLibSound.sh
+++ Speculator/BuildLibSound.sh
@@ -24,23 +24,5 @@ cmake ..
 make
 cd ..
 
-# Check if the user wants to build for Windows
-if [ "$1" == "--build-windows" ]; then
-    # Install MinGW-w64 if it's not installed
-    if ! command_exists x86_64-w64-mingw32-gcc; then
-        echo "MinGW-w64 not found. Installing MinGW-w64..."
-        brew install mingw-w64
-    else
-        echo "MinGW-w64 already exists."
-    fi
-
-    # Build for Windows (64-bit)
-    echo "Building for Windows (64-bit)..."
-    mkdir -p build-windows && cd build-windows
-    cmake .. -DCMAKE_C_COMPILER=x86_64-w64-mingw32-gcc -DCMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++
-    make
-    cd ..
-fi
-
 echo "Build process complete."
 
