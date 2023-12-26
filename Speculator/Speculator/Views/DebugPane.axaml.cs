e1ead71ce0abaa29554a9f07bde8da152f89dbfe
Dean Edis <dean.edis@gmail.com>
2023-12-26 21:33:21 +0000

    Other: Move debug UI into its own user control.

---
+++ Speculator/Speculator/Views/DebugPane.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Speculator.Views;
+
+public partial class DebugPane : UserControl
+{
+    public DebugPane()
+    {
+        InitializeComponent();
+    }
+}
