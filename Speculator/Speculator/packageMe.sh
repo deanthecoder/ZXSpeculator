5f2d06a154501a13292dd6c3a01c4ba1ec400035
Dean Edis <dean.edis@gmail.com>
2024-01-14 16:33:13 +0000

    Other: Mac .dmg file now automatically created.

--- Speculator/Speculator/packageMe.sh
+++ Speculator/Speculator/packageMe.sh
@@ -9,15 +9,15 @@ BUNDLE_NAME="${APP_NAME}.app"
 IDENTIFIER="com.deanedis.zxspeculator"
 
 # Step 1: Publish the application
-rm -rf bin/Release/net7.0/osx-arm64/publish/ 
-dotnet publish -c Release -r osx-arm64 --self-contained true -property:Configuration=Release -p:UseAppHost=true
+rm -rf bin/Release/net7.0/osx-x64/publish/ 
+dotnet publish -c Release -r osx-x64 --self-contained true -property:Configuration=Release -p:UseAppHost=true
 
 # Step 2: Create the app bundle structure
 mkdir -p "$BUNDLE_NAME/Contents/MacOS"
 mkdir -p "$BUNDLE_NAME/Contents/Resources"
 
 # Step 3: Move the published files into the app bundle
-cp -r bin/Release/net7.0/osx-arm64/publish/* "$BUNDLE_NAME/Contents/MacOS/"
+cp -r bin/Release/net7.0/osx-x64/publish/* "$BUNDLE_NAME/Contents/MacOS/"
 
 # Step 4: Delete any .pdb files
 find "$BUNDLE_NAME/Contents/MacOS/" -name '*.pdb' -delete
@@ -53,3 +53,22 @@ EOF
 cp Assets/Icon.icns "$BUNDLE_NAME/Contents/Resources/Icon.icns"
 
 echo "App bundle $BUNDLE_NAME created."
+
+# Step 6: Create a .dmg file from the app bundle
+
+DMG_NAME="${APP_NAME}_${APP_VERSION}.dmg"
+VOLUME_NAME="${APP_NAME} ${APP_VERSION_SHORT}"
+
+# Remove any existing .dmg file with the same name
+rm -f "$DMG_NAME"
+
+# Create a temporary disk image and format it
+hdiutil create -ov -volname "$VOLUME_NAME" -fs HFS+ -srcfolder "$BUNDLE_NAME" -format UDRW "temp_$DMG_NAME"
+
+# Convert the temporary disk image to a compressed .dmg file
+hdiutil convert "temp_$DMG_NAME" -format UDZO -o "$DMG_NAME"
+
+# Remove the temporary disk image
+rm -f "temp_$DMG_NAME"
+
+echo "DMG file $DMG_NAME created."
