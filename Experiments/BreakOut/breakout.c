c1bc781375cacecedb15353b1fd519162399f9f6
Dean Edis <dean.edis@gmail.com>
2024-03-11 09:41:13 +0000

    Other: 'Breakout' logo tweaks.

--- Experiments/BreakOut/breakout.c
+++ Experiments/BreakOut/breakout.c
@@ -1,95 +1,116 @@
 // zcc +zx -lndos -create-app -DAMALLOC -o breakout breakout.c
+// zcc +zx -lndos -DAMALLOC -o breakout.bin breakout.c
 #include <conio.h>
 #include <input.h>
 #include <graphics.h>
 #include <math.h>
 #include <arch/zx/spectrum.h>
 
-#define L 22
-#define WALL 2
-#define DARK_C  INK_BLUE
-#define LIGHT_C  INK_CYAN
-
+/** Generic functions */
 static unsigned char* attrMem = 22528;
-static uint8_t* grid;
+static uint8_t ticks = 0;
 
-static void plot_shade(uint8_t x, uint8_t y, uint8_t density)
+static void plot_shade(uint8_t x, uint8_t y, uint8_t density255)
 {
-    if (density == 0)
-        return;
-    if (density >= 100)
-    {
+    // Plot this pixel based on the density.
+    if (density255 == 255 || (uint8_t)rand() < density255)
         plot(x, y);
-        return;
-    }
-
-    // Convert density to a range from 0 to 100 for comparison
-    uint8_t threshold = (uint8_t)((density * 255) / 100);
-
-    // Use rand() to decide whether to plot this pixel based on the density
-    if ((rand() % 256) < threshold) {
-        plot(x, y); // Plot if within the density threshold
-    }
 }
 
-static void c_plot_shade(uint8_t x, uint8_t y, uint8_t density)
+static void c_plot_shade(uint8_t x, uint8_t y, uint8_t density255)
 {
     x <<= 2;
     y <<= 2;
 
     for (uint8_t i = 0; i < 4; ++i)
     {
+        const uint8_t py = y + i;
         for (uint8_t j = 0; j < 4; ++j)
-            plot_shade(x + i, y + j, density);
+            plot_shade(x + i, py, density255);
     }
 }
 
 static void drawLogo(uint8_t x, uint8_t y)
 {
-    const char* logo[23] =
+    if (ticks > 30)
+        return;
+
+    if (ticks == 0)
     {
-        " **********  ",
-        "..***....*** ",
-        " .***   ..***",
-        " .***    .***",
-        " .***    *** ",
-        " **********  ",
-        "..........   ",
-        "             ",
-        " *********** ",
-        ".*...***...* ",
-        ".   .***  .  ",
-        "    .***     ",
-        "    .***     ",
-        "    *****    ",
-        "   .....     ",
-        "             ",
-        "   ********* ",
-        "  ***.....***",
-        " ***     ... ",
-        ".***         ",
-        "..***     ***",
-        " ..********* ",
-        "  .........  "
-    };
-    const uint8_t c[] = { WHITE, LIGHTGRAY, LIGHTCYAN, LIGHTGREEN, LIGHTMAGENTA, LIGHTRED, RED, RED };
-    for (uint8_t j = 0; j < 23 * 2; ++j)
-    {
-        textcolor(c[(j >> 1) % 8]);
-        for (uint8_t i = 0; i < 13; ++i)
+        // Seed the pixels - Initially all black.
+        const char* logo[23] =
         {
-            switch (logo[j >> 1][i])
+            " **********",
+            "..***....***",
+            " .***   ..***",
+            " .***    .***",
+            " .***    ***",
+            " **********",
+            "..........",
+            "",
+            " ***********",
+            ".*...***...*",
+            ".   .***  .",
+            "    .***",
+            "    .***",
+            "    *****",
+            "   .....",
+            "",
+            "   *********",
+            "  ***.....***",
+            " ***     ...",
+            ".***",
+            "..***     ***",
+            " ..*********",
+            "  ........."
+        };
+
+        for (uint8_t j = 0; j < 23 * 2; ++j)
+        {
+            textcolor(BLACK);
+            uint8_t* l = logo[j >> 1];
+            const uint8_t w = strlen((const char*)l);
+            const uint8_t py = j + y;
+            for (uint8_t px = x; px < x + w; ++px)
             {
-                case '*':
-                    c_plot(i + x, j + y);
-                    break;
-                case '.':
-                    c_plot_shade(i + x, j + y, 15);
-                    break;
+                switch (*l++)
+                {
+                    case '*':
+                        c_plot(px, py);
+                        break;
+                    case '.':
+                        c_plot_shade(px, py, 40);
+                        break;
+                }
             }
         }
+
+        ++ticks;
+        return;
     }
+
+    // Apply the color over time.
+    y >>= 1;
+    x >>= 1;
+
+    static const uint8_t logoC[] = { INK_WHITE + BRIGHT, INK_WHITE, INK_CYAN, INK_GREEN, INK_MAGENTA, INK_MAGENTA, INK_RED, INK_RED };
+    uint8_t j = ticks, i = 0;
+    while (j >= 0 && i < 7)
+    {
+        attrMem[((j + y) << 5) + x + i] = logoC[j & 0x07];
+        --j; ++i;
+    }
+
+    ++ticks;
 }
+
+/** App-specific **/
+#define L 22
+#define WALL 2
+#define DARK_C  INK_BLUE
+#define LIGHT_C  INK_CYAN
+
+static uint8_t* grid;
 
 static unsigned int gridToScreen(uint8_t x, uint8_t y)
 {
@@ -198,12 +219,11 @@ void main()
     textbackground(BLACK);
     textcolor(WHITE);
     clrscr();
+    ticks = 0;
 
     // Footer.
     gotoxy(40, 23);
     printf("Breakout (@DeanTheCoder)");
-
-    drawLogo(3, 1);
 
     // Init the grid.
     grid = malloc(L * L);
@@ -238,6 +258,9 @@ void main()
     in_GetKeyReset();
     while (!in_GetKey())
     {
+        // Refresh the logo.
+        drawLogo(3, 1);
+
         // Move ballz.
         for (uint8_t phase = 0; phase < 4; ++phase)
         {
